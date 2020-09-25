8fe16690c9e9ae3b20e9a7024f302bd2ea3701a7
wangzn <wangzhaonian@foxmail.com>
2020-09-25 23:45:09 +0800

    update

--- typeTrans.c
+++ typeTrans.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+typedef struct StructDemo
+{
+   char name[2];
+   int num;
+   double score;
+}StructDemo;
+
 int main()
 {
    int sum = 17, count = 5;
    double mean;
-
+   StructDemo std = {'a',0};
+   printf("%c",std.name);
    mean =  sum / count;
    printf("Value of mean : %f\n", mean );
 
